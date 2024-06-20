cfe0802968390dcb8249ba18319d6b0e13182537
yamil.lanz <yamil.lanz@externos-cl.cencosud.com>
2024-06-20 10:25:21 -0400

    fail deleting

--- functions/src/shared/FirebaseConfigFactory.ts
+++ functions/src/shared/FirebaseConfigFactory.ts
@@ -1,16 +1,25 @@
 import * as admin from 'firebase-admin';
 /* eslint-disable require-jsdoc */
 
+// apiKey: "AIzaSyAl_96zKawtX1mILmC2axYgTr_MksLDx5Y",
+// authDomain: "todo-app-c9ea4.firebaseapp.com",
+// projectId: "todo-app-c9ea4",
+// storageBucket: "todo-app-c9ea4.appspot.com",
+// messagingSenderId: "180845934620",
+// appId: "1:180845934620:web:49ac1e377113ee8c82c7d9"
+
+// const serviceAccount = require('../../config/todo_app.json');
+import * as serviceAccount from '../../config/todo_app.json';
 export class FirebaseConfigFactory {
   static create() {
-    const serviceAccount = {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    };
+    // const serviceAccount = {
+    //   projectId: process.env.FIREBASE_PROJECT_ID || 'todo-app-c9ea4',
+    //   privateKey: 'AIzaSyAl_96zKawtX1mILmC2axYgTr_MksLDx5Y',
+    //   clientEmail: 'yamil.w.lanz@gmail.com',
+    // };
 
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
       databaseURL: process.env.FIREBASE_DATABASE_URL,
     });
 
--- functions/src/todos/Infraestructure/routes/todos/index.ts
+++ functions/src/todos/Infraestructure/routes/todos/index.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { TodoController } from '../../controllers/todo.controller';
 import { TodoUseCase } from '../../../Application/TodoUseCase';
 import { TodoMockRepository } from '../../reposities/TodoMockRepository';
+// import { FireBaseTodoRepository } from '../../FireBaseTodoRepository';
 
 // eslint-disable-next-line new-cap
 const routesTodo = Router();
 const respoMock = new TodoMockRepository();
+// const todoFirebaseRepository = new FireBaseTodoRepository();
+// const todoUseCase: TodoUseCase = new TodoUseCase(todoFirebaseRepository);
 const todoUseCase: TodoUseCase = new TodoUseCase(respoMock);
 const todoCtrl: TodoController = new TodoController(todoUseCase);
 
