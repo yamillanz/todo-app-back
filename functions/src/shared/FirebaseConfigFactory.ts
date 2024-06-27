51715143894e64de22d143a4780fecc385b05a8a
yamil.lanz <yamil.lanz@externos-cl.cencosud.com>
2024-06-27 16:02:15 -0400

    modified gitignore

--- functions/src/shared/FirebaseConfigFactory.ts
+++ functions/src/shared/FirebaseConfigFactory.ts
@@ -1,20 +1,7 @@
 /* eslint-disable require-jsdoc */
-// import * as admin from 'firebase-admin';
-
-// import * as serviceAccount from '../config/todo_app.json';
-// export class FirebaseConfigFactory {
-//   static create() {
-//     admin.initializeApp({
-//       credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-//       databaseURL: process.env.FIREBASE_DATABASE_URL,
-//     });
-
-//     return admin;
-//   }
-// }
 
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '../config/todo_app.json';
+import * as serviceAccount from '../account/todo_app.json';
 
 export class FirebaseConfigFactory {
   private static instance: admin.app.App | null = null;
